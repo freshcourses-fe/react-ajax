ce75b0c7570d29d30db613cd60ca53aca279cbd4
Kirill <freshjs.ftd.@gmail.com>
2022-05-04 19:12:06 +0300

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
 import './reset.css';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import UserLoader from './components/UserLoader';
 
 function App() {
-  return <UserLoader />;
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about/test">
+          <AboutTest />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
+
+const Home = () => <h1>HOME PAGE</h1>;
+const AboutTest = () => <h1>ABOUT Test PAGE</h1>;
+const About = () => <h1>ABOUT PAGE</h1>;
+const Contacts = () => <h1>CONTACTS PAGE</h1>;
 
 export default App;
