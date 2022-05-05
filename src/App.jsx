f024db5affca86c88014283f85cb46b0a829e0be
Kirill <freshjs.ftd.@gmail.com>
2022-05-05 17:30:08 +0300

    Remove unused inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './reset.css';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import UserLoader from './components/UserLoader';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
-import Input from './components/input';
 import ContactsPage from './pages/ContactsPage';
 
 function App() {
@@ -14,7 +12,7 @@ function App() {
     <BrowserRouter>
       {/* так обычно не делают (именно факт того что компонент а не страница) */}
       <Route exact component={Header} />
-      <Input />
+
       <Switch>
         <Route path="/" exact>
           {(utilProps) => <HomePage {...utilProps} />}
